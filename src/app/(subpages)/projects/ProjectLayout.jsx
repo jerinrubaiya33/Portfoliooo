1cf8e448b74800c34c957b889effbf01c938678e
Rubaiya Khan Jerin <163726222+jerinrubaiya33@users.noreply.github.com>
2025-08-25 12:01:50 +0600

    ProjectLayout.jsx

--- src/app/(subpages)/projects/ProjectLayout.jsx
+++ src/app/(subpages)/projects/ProjectLayout.jsx
@@ -3,7 +3,7 @@ import React, { useMemo } from "react";
 import styles from './ProjectLayout.module.css';
 
 const buttonColors = [
-  "#6a4c93", "#00d1ff", "#f44336", "#ff0068", "#ff9800", "#b2ff00", "#ffc100"
+  "#6a4c93", "#00AED4", "#f44336", "#ff0068", "#ff9800", "#9ADB00", "#ffc100"
 ];
 
 const ProjectLayout = ({ name, description, date, demoLink, index }) => {
