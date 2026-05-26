4dabf9cb647c9fdb286aaac9e7273cebaeb90c86
jerinrubaiya33 <jerinrubaiya11@gmail.com>
2026-05-26 15:28:54 +0600

    Deleted unnecessery files

--- src/app/(subpages)/projects/layout.js
+++
@@ -1,8 +0,0 @@
-
-export default function SubPagesLayout({ children }) {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center py-18 px-18">
-      {children}
-    </main>
-  );
-}
